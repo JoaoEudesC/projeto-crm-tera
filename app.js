7e4c58f56a5716b2303da4b94d2f9a2d04ec09d7
zigmons <rafael_sousa87@hotmail.com>
2022-11-09 18:34:41 -0300

    Arrumado area de telefone da home e JS

--- app.js
+++ app.js
@@ -14,25 +14,55 @@ function btn(){
     let nome = document.getElementById('name')
     let email = document.getElementById('email')
     let telefone = document.getElementById("telefone")
+    let texto = document.getElementById("texto_contato")
     let nome1 = nome.value
     let email1 = email.value
     let telefone1 = telefone.value
+    let texto1 = texto.value
+    let div = document.getElementById('infos')
+    let regex = /^[0-9]+$/;
 
-    
-    if(nome1.match(/^[a-zA-Z_ ]*$/)){
-        if (nome1 == "") { 
-            let div = document.getElementById('infos')
-            let h4 = div.innerHTML = `<h4>Por favor, preencha seu nome!</h4>`
-        }else if(email1 == "" || telefone1 ==""){
-            let div = document.getElementById('infos')
-            let h4 = div.innerHTML = `<h4 style="color: red">Por favor, preencha os campos de e-mail, telefone e digite sua necessidade!</h4>`
-        }else{
-            let div = document.getElementById('div1')
-            let h4 = div.innerHTML = `<h4>Olá ${nome1}, obrigado pelo seu contato. Assim que analisarmos suas necessidades, entraremos em contato através do telefone ${telefone1} ou
-            do endereço de e-mail ${email1}`
-        }
-    }else{
-        let div = document.getElementById('infos')
-        let h4 = div.innerHTML = `<h4 style="color: red">Nome deve conter apenas letas, sem numeros!</h4>`
+    if (nome1.match(regex)){
+        return div.innerHTML = `<h4 style="color: red">Nome deve conter apenas letas, sem numeros!</h4>`
     }
+
+    if(nome1==""){
+        return div.innerHTML = `<h4>Por favor, preencha seu nome!</h4>`
+    }
+
+    if(email1 == "" || telefone1 ==""){
+        return div.innerHTML = `<h4 style="color: red">Por favor, preencha os campos de e-mail, telefone e digite sua necessidade!</h4>`
+    }
+
+    if(texto1===""){
+        return div.innerHTML = `<h4 style="color: red">Por favor, preencha o campo de texto!</h4>`
+    }
+
+    else{
+        window.alert( `Olá ${nome1}, obrigado pelo seu contato. Assim que analisarmos suas necessidades, entraremos em contato através do telefone ${telefone1} ou do endereço de e-mail ${email1}`)
+        }
 }
+
+
+// Conferir configuração de caracteres do telefone
+$(document).ready(function(){
+    $('body').on('focus', '.telefone', function(){
+        var maskBehavior = function (val) {
+            return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
+        },
+        options = {
+            onKeyPress: function(val, e, field, options) {
+                field.mask(maskBehavior.apply({}, arguments), options);
+
+                if(field[0].value.length >= 14){
+                    var val = field[0].value.replace(/\D/g, '');
+                    if(/\d\d(\d)\1{7,8}/.test(val)){
+                        field[0].value = '';
+                        alert('Telefone Invalido');
+                    }
+                }
+            }
+        };
+        $(this).mask(maskBehavior, options);
+    });
+});
