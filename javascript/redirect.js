60f83391a2d9d0592ca577c7364415e39c78f87d
zigmons <rafael_sousa87@hotmail.com>
2022-12-11 01:59:17 -0300

    final

--- javascript/redirect.js
+++ javascript/redirect.js
@@ -1,6 +1,9 @@
-$(document).ready(function () {
-    // Handler for .ready() called.
-    window.setTimeout(function () {
-        location.href = "https://joaoeudesc.github.io/projeto-crm-tera/";
-    }, 5000);
-});
+
+var count = 10
+setInterval(function(){
+    count--;
+    document.getElementById('countDown').innerHTML = `Você será redirecionado para a pagina inicial em ${count} segundos`;
+    if (count == 0) {
+        window.location = 'https://joaoeudesc.github.io/projeto-crm-tera'; 
+    }
+},1000)
