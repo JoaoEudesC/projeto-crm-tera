33ac72536cc470f84720425b2655573011bea3f1
joaoeudes2501 <joaoeudes2501200@gmail.com>
2023-02-11 09:02:22 +0000

    organização do front com o back

---
+++ frontend/javascript/appcadastro.js
@@ -0,0 +1,239 @@
+
+//Seleção de elementos  
+    
+    
+    
+    
+    
+    //Inputs
+        let senha = document.getElementsByTagName("input")[2]
+        let confirmarSenha = document.getElementsByTagName("input")[3]
+        
+        
+        
+    //Formulario
+        const form = document.getElementById('Form')
+    
+    
+    //Adição de elementos ao localStorage
+
+
+
+
+
+
+
+    
+
+    
+    
+
+
+
+
+
+//Mapeando a tecla enter para enviar o formulario
+    document.addEventListener('keypress' , (e)=>{
+        if(e.key === 'enter')
+        botaoCadastro.click();
+    })
+
+//Mapeando a tecla capslock para avisar ao usuario se a tecla está ativada ou não ao inserir a senha
+    senha.addEventListener('keyup' , (e)=>{
+        const textoAlerta = document.getElementById('uperCase')
+        if(e.getModifierState('CapsLock')){
+            textoAlerta.style.display = 'block'
+        }
+        else{
+            textoAlerta.style.display ='none'
+        }
+    })
+    
+    confirmarSenha.addEventListener('keyup' , (e)=>{
+        const uperCaseConfirm = document.getElementById('uperCaseConfirm')
+        if(e.getModifierState('CapsLock')){
+            uperCaseConfirm.style.display = 'block'
+        }
+        else{
+            uperCaseConfirm.style.display = 'none'
+        }
+    })
+    
+    
+
+
+
+
+
+
+
+// aplicação de api/cep
+    const cep = document.querySelector("#cep");
+    
+    const options = {
+        method:"get",
+        mode: "cors",
+        cache:"default"
+    }
+    
+    const showdata = (result)=>{
+        for(const campo in result){
+            if(document.querySelector("#"+campo)){
+                document.querySelector("#"+campo).value =result[campo];
+            }
+        }
+    }
+    
+    cep.addEventListener("blur", (e) => {
+            let search = cep.value.replace("-", "");
+            console.log(search);
+            fetch( `https://viacep.com.br/ws/${search}/json/`, options)
+            .then(response => {
+            response.json()
+            .then(data=>{
+                showdata(data)
+            })
+        })
+        
+        .catch(e=>{ 
+            console.log("Falha de conexão " + e)
+        })
+    
+    }) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // => Ligação da nossa api do banco de dados com o nosso frontend
+
+
+//criação da função de submeter o formulário para o backend
+
+const init = ()=>{
+    const botao =  document.getElementById("cadastro")
+    botao.addEventListener("click" , submitForm)
+};
+
+const submitForm = async (e) =>{
+    
+    e.preventDefault();
+    let senha = document.getElementById("senha")
+    let confirm = document.getElementById("confirmarSenha").value
+    let valueSenha = senha.value
+    let MessageError = document.getElementById("MessageError")
+    
+    if(valueSenha != confirm  ){
+        valueSenha = ""
+        confirm = ""
+        senha.focus()
+        return MessageError.style.display ="block"
+    }
+    
+    const data = accessData()
+    const url = "http://localhost:3000/users/create"
+    if(!data){
+        return console.log("Dados nao estão corretos")
+    }
+    
+    // credentials => "same-origin" é usada no contexto de requisições HTTP com o Fetch API e indica que as credenciais (cookies e headers de autenticação) devem ser incluídas na requisição se o URL da requisição for da mesma origem do aplicativo cliente. Em outras palavras, isso permite que a requisição acesse recursos protegidos que dependem de autenticação da mesma origem. Se o URL da requisição for de uma origem diferente, as credenciais não serão incluídas na requisição.
+    
+    //criação do metodo de requisição para submeter os dados do formulário de forma correta
+    const Fetch = {
+        method:"POST",
+        body:JSON.stringify(data),
+        headers:{
+            "content-Type": "application/json",
+            
+        },
+        credentials: "same-origin"
+    }
+    
+    const requisition = await fetch(url, Fetch) 
+    .then((response)=>{
+        if(response.status === 201){
+            return window.location.href = "indexlogin.html"
+
+        }
+        else{
+            return alert("Usuário não cadastrado , tente novamente")
+        }
+    })
+    .then((data)=>{
+        console.log(data)
+    
+    })
+    .catch((error)=>{
+        return console.log("erro na requisição" + error)
+    })
+
+    
+};
+
+
+//Função para acessar os valores enviados através dos inputs e retorna-los a variavel associada ao data, para ser enviada ao back a partir do fetch
+    const accessData = ()=>{
+        return{
+                nome:document.getElementById("NomeCompleto").value,
+                email:document.getElementById("email").value,
+                senha:document.getElementById("senha").value,
+                cep:document.getElementById("cep").value,
+                logradouro:document.getElementById("logradouro").value,
+                bairro:document.getElementById("bairro").value,
+                localidade:document.getElementById("localidade").value,
+                uf:document.getElementById("uf").value,
+        }
+    }
+
+
+//Função que diz respeito ao envio do formulário com o evento submmit
+init();
+
+
+//Função que diz respeito a pegar todos os valores dos inputs e retorna-los dentro da variável data para que possa ser enviado ao backend(Atenção que a chamada dos valores dos inputs tem que condizer com o schema montando no backend)
+accessData();
+
+
+
+
+
+//Observação importante => colocar a função que o fetch está sempre como se fosse uma função assicrona e se utilizar o if lembrar de sempre colocar um return no uso de validação , tudo que for antes do return não será executado , ou seja, o return sempre por ultimo
+//como é o caso que vemos na requisição de login e aqui na validação da senha e confirmar a senha
+
+
+
+
+
+
+
+    
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ frontend/javascript/login.js
@@ -0,0 +1,140 @@
+//Seleção de Elementos
+    let eye = document.getElementById('show_Senha')
+    const Senha = document.getElementsByTagName('input')[1]
+    const email = document.getElementsByTagName('input')[0].value
+    const botaoLogin = document.getElementById('entrar')
+    const form = document.getElementById('form')
+
+
+    
+    //pegando value do input de senha 
+    const senha = Senha.value
+
+
+
+
+//Mapeando o enter para o envio de formulário atravês do keypress
+    document.addEventListener('keypress' , (e)=>{
+        if(e.key === 'enter')
+        botaoLogin.click();
+    })
+
+
+
+//Mapeando a tecla capslock para avisar ao usuario se a tecla está ativada ou não ao inserir a senha
+    Senha.addEventListener('keyup' , (e)=>{
+        const textAlert = document.getElementById('textAlert')
+        if(e.getModifierState('CapsLock')){
+            textAlert.style.display='block'
+        }
+        else{
+            textAlert.style.display = 'none'
+        }
+    })
+
+//Icone do olho que esconde  e mostra a senha
+    eye.addEventListener("click" , ()=>{
+        if(Senha.type == "password"){
+            Senha.type = 'text'
+        }
+        else{
+            Senha.type = 'password'
+        }
+    })
+
+
+
+
+
+//Autenticaçao e login => Ligação da nossa aplicação com o backend , a parte de login , da rota login, autenticação e login
+    const init = () => form.addEventListener('submit' , async (e)=>{
+        e.preventDefault();
+        const data = accessData(); 
+        const url = "http://localhost:3000/users/login"
+        const MessageError = document.getElementById("MessageAlert")
+        
+        if(!data){
+            return console.log("Dados errados ")
+        }
+        
+        const Fetch = {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: {
+                "Content-Type": "application/json",
+            },
+            credentials: "same-origin",
+        }
+        
+        await fetch(url , Fetch)
+        
+        .then((response)=>{
+            if(response.status === 200){
+                window.location.href = "UsuárioCadastrado.html"
+            }
+            else{
+                return MessageError.style.display = "block"
+            }
+            
+        })
+        .then((data)=>{
+            console.log(data)
+            
+        })
+    
+        .catch((error)=>{
+            return console.log("erro na requisição" + error)
+        })
+
+        
+
+    });
+
+
+
+const accessData = ()=>{
+    return{
+        senha: document.getElementsByTagName('input')[1].value,
+        email: document.getElementsByTagName('input')[0].value
+    };
+};
+
+//Chamada da função que diz respeito ao envio dos valores do inut para a variavel data que vai ser enviada ao backend através do post ( atenção os valores dos inputs tem que está associados ao schema criado no backend)
+accessData();
+
+//Chamada da função que diz respeito ao envio do formulário
+init();
+
+
+
+
+
+
+//Observação importante => O que vier depois de um return não será executado , por isso em uma função if coloque o return por ultimo e as funções a serem executadas primeiro , como vimos no caso do primeiro .then e na validação de confirmação de senha do cadastro
+//Repare que utilizamos uma função if dentro do then , para fazer a validação do usuário baseado no statusCode, podemos fazer isso no cadastro tbm 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
