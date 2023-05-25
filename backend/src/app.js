0cefb4190f2ecebbcec23cd5b15f840e529cdaa1
joaoeudes2501 <joaoeudes2501200@gmail.com>
2023-05-25 22:20:15 +0100

    Concerto de url

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,20 +1,13 @@
 const cors = require("cors");
 const express = require("express");
-const swaggerUI = require("swagger-ui-express");
 
 const db = require("./config/database");
 const router = require("./routes/userRoutes");
-const swaggerFile = require("./swagger.json");
 
 require("dotenv").config();
 
 const app = express();
 app.use(cors());
-
-const port = process.env.PORT;
-console.log(port);
-
-app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
 
 app.use(express.json());
 
