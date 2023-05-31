422febdc5a9819676bf29348198f963a59a54fdc
joaoeudes2501 <joaoeudes2501200@gmail.com>
2023-05-31 17:57:25 +0100

    Inicio de testes unitários

---
+++ backend/src/tests/unitTests/CreateUser.spec.js
@@ -0,0 +1,9 @@
+describe("Somar dois números", () => {
+    it("Deve ser possível somar dois números", () => {
+        const number1 = 10;
+        const number2 = 10;
+        const soma = number1 + number2;
+
+        expect(soma).toBe(20);
+    });
+});
